f00b6ae4e76061d9496b2b79d34db5afa7572d05
Andy Li <andy@onthewings.net>
2017-07-23 22:36:01 +0800

    fixed compilation with OCaml 4.05
    caused by new functions added to the standard library's Map module

--- test.ml
+++ test.ml
@@ -19,7 +19,36 @@ let test empty add mem =
   Random.init seed;
   for i = 0 to 999 do assert (mem (Random.int max_int) s) done
 
+let list_to_map lst =
+  let rec loop l map = match l with
+  | [] -> map
+  | item :: ls -> loop ls (Ptmap.add item true map)
+  in
+  loop lst Ptmap.empty
+
+let test_find_first =
+  let k,v = Ptmap.find_first (fun i -> i > 3) (list_to_map [3; 1; 2; 4; 6; 5]) in
+  assert (k = 4)
+
+let test_find_first_opt =
+  match Ptmap.find_first_opt (fun i -> i > 3) (list_to_map [3; 1; 2; 4; 6; 5]) with
+  | Some (4, v) -> assert true
+  | _ -> assert false
+
+let test_find_last =
+  let k,v = Ptmap.find_last (fun i -> i < 4) (list_to_map [3; 1; 2; 4; 6; 5]) in
+  assert (k = 3)
+
+let test_find_last_opt =
+  match Ptmap.find_last_opt (fun i -> i < 4) (list_to_map [3; 1; 2; 4; 6; 5]) with
+  | Some (3, v) -> assert true
+  | _ -> assert false
+
 let main () =
-  test Ptmap.empty Ptmap.add Ptmap.mem
+  test Ptmap.empty Ptmap.add Ptmap.mem;
+  test_find_first;
+  test_find_first_opt;
+  test_find_last;
+  test_find_last_opt
 
 let () = main ()
